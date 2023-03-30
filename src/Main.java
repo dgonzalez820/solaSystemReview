ce683be59c56c75d4d062684c3fc6db00af7f708
Diego Gonzalez <Dministry07@gmail.com>
2023-03-29 20:26:16 -0400

    the basic of solar

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        solarSystem solar= new solarSystem();
+
+        solar.planet="2";
+        solar.moon="2";
+        solar.sun="1";
+
+
+    }
+}
